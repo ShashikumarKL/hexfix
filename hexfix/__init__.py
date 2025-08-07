12286f30a4befb8407f1f6dc3dc9d7151ff57f6f
SHASHIKUMAR K L <38139647+ShashikumarKL@users.noreply.github.com>
2025-08-07 21:18:07 +0530

    Test validate_srec_file

--- hexfix/__init__.py
+++ hexfix/__init__.py
@@ -1,6 +1,6 @@
 """Convenience imports for hexfix package."""
 
-from .checksum import crc32_for_address_range
+from .checksum import crc32_for_address_range, validate_srec_file
 from .merge import merge_srec_files
 from .edit import extract_range_srec, relocate_range_srec
 
@@ -9,4 +9,5 @@ __all__ = [
     "merge_srec_files",
     "extract_range_srec",
     "relocate_range_srec",
+    "validate_srec_file",
 ]
