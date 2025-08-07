f6f4aed2bbed4cb08c1cad84e1fda48c12c27064
SHASHIKUMAR K L <38139647+ShashikumarKL@users.noreply.github.com>
2025-08-07 21:18:18 +0530

    Add SREC to binary conversion utility and tests

--- hexfix/__init__.py
+++ hexfix/__init__.py
@@ -3,10 +3,12 @@
 from .checksum import crc32_for_address_range
 from .merge import merge_srec_files
 from .edit import extract_range_srec, relocate_range_srec
+from .convert import srec_to_binary
 
 __all__ = [
     "crc32_for_address_range",
     "merge_srec_files",
     "extract_range_srec",
     "relocate_range_srec",
+    "srec_to_binary",
 ]
